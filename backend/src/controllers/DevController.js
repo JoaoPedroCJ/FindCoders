bf3590f814eae964b917e83d01fb1fce5f2b127a
João Pedro <joaopedrocj@gmail.com>
2020-01-17 11:47:26 -0300

    Socket IO

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../Utils/parseStringAsArray");
 const findGithub = require("../Utils/FindGithub.js");
+const { findConnections, sendMessage } = require("../websocket");
 
 // index, show, store, update, destroy
 
@@ -37,6 +38,16 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      // Filtrar as conexões que estão há no máximo 10km de distância
+      // e que o ovo dev tenha pelo menos uma das tecnologias filtradas
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      )
+
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
 
     return response.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require('http');
 const routes = require("./routes");
+const { setupWebsocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(
   "mongodb+srv://omnistack10:omnistack10@cluster0-aqd5u.mongodb.net/omnistack?retryWrites=true&w=majority",
@@ -18,4 +23,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
